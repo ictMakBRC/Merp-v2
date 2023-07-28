9fceec3ac168b067f8a5bfbf5e01bb0ba930ee0a
TimothyMuyingo <muytimothy@gmail.com>
2023-07-29 02:30:39 +0300

    setup changes

---
+++ resources/views/livewire/layouts/partials/inc/human-resource/inc/leaves-nav.blade.php
@@ -0,0 +1,23 @@
+<li class="nav-item">
+    <a class="nav-link" href="#employeeLeaveMenu" data-bs-toggle="collapse" role="button" aria-expanded="false"
+        aria-controls="employeeLeaveMenu">
+        Leaves
+    </a>
+    <div class="collapse " id="employeeLeaveMenu">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a href="{{ route('human-resource-stations') }}" class="nav-link ">Requests</a>
+            </li>
+            <!--end nav-item-->
+            <li class="nav-item">
+                <a href="analytics-reports.html" class="nav-link ">My Delegations</a>
+            </li>
+
+            <li class="nav-item">
+                <a href="analytics-reports.html" class="nav-link ">Departmental Requests</a>
+            </li>
+            <!--end nav-item-->
+        </ul>
+        <!--end nav-->
+    </div>
+</li>
---
+++ resources/views/livewire/layouts/partials/inc/human-resource/inc/reports-nav.blade.php
@@ -0,0 +1,14 @@
+<li class="nav-item">
+    <a class="nav-link" href="#employeeReportsMenu" data-bs-toggle="collapse" role="button" aria-expanded="false"
+        aria-controls="employeeReportsMenu">
+        Reports
+    </a>
+    <div class="collapse " id="employeeReportsMenu">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a href="#" class="nav-link ">Reports</a>
+            </li>
+        </ul>
+        <!--end nav-->
+    </div>
+</li>
