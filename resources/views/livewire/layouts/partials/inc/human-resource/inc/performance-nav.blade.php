c4870f807454999c562106adcb21c6d16a190c40
Davis <davisag67@gmail.com>
2023-09-25 22:23:18 +0300

    update the warning side nav

--- resources/views/livewire/layouts/partials/inc/human-resource/inc/performance-nav.blade.php
+++ resources/views/livewire/layouts/partials/inc/human-resource/inc/performance-nav.blade.php
@@ -3,7 +3,7 @@
         aria-controls="performanceMenu"><i class="ti ti-file-analytics me-2"></i>
         Performance
     </a>
-    <div class="collapse {{isLinkActive(['appraisals.show'], 'show')}}" id="performanceMenu">
+    <div class="collapse {{isLinkActive(['appraisals.show', 'warnings.show'], 'show')}}" id="performanceMenu">
         <ul class="nav flex-column">
             <li class="nav-item">
                 <a href="#appraisalsMenu" class="nav-link" data-bs-toggle="collapse" role="button" aria-expanded="false"
@@ -41,10 +41,16 @@
                     aria-controls="warningsMenu">
                     Warning
                 </a>
-                <div class="collapse" id="warningsMenu">
+                <div class="collapse {{isLinkActive(['warnings.show'], 'show')}}" id="warningsMenu">
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{route('warnings.create')}}">Create</a>
+                            <a class="nav-link {{isLinkActive(['warnings.show'], 'active')}}"
+                                href="{{route('warnings.create')}}">
+                                @if(showWhenLinkActive('warnings.show'))
+                                <span>Show</span>
+                                @else
+                                <span>New</span>
+                                @endif</a>
                         </li>
                         <!--end nav-item-->
                         <li class="nav-item">
