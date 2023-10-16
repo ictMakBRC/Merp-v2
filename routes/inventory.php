2ef0c47d8fe8c6d6f8c5515ec87de2c6067c3f12
okello23 <benbyron24@gmail.com>
2023-10-16 16:45:20 +0300

    added inventory roles & permissions to rolespermissions_seeder

--- routes/inventory.php
+++ routes/inventory.php
@@ -16,29 +16,32 @@ use App\Http\Livewire\Inventory\Requisitions\GeneralRequisitionsComponent;
 use App\Http\Livewire\Inventory\Requisitions\ConsumptionBasedRequisitionsComponent;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'inventory'], function () {
+Route::group(['prefix' => 'inventory','middleware' => ['permission:access_inventory_module']], function () {
+
+
 
   Route::get('inventory-home', InvUnitOfMeasuresComponent::class)->name('inventory-home');
 
   Route::get('dashboard', InventoryMainDashboardComponent::class)->name('inventory-dashboard');
-  Route::group(['prefix' => 'manage'], function () {
-    Route::get('commodities', CommoditiesComponent::class)->name('inventory-commodities');
-    Route::get('department-items', DepartmentItemsComponent::class)->name('department-items');
-    Route::get('stores/sections', InvStorageSectionsComponent::class)->name('inventory-sections');
-    Route::get('stores/sections/bins', InvStorageSubSectionsComponent::class)->name('inventory-storage_bins');
-    Route::get('unit_of_measures', InvUnitOfMeasuresComponent::class)->name('inventory-unit_of_measures');
-  });
 
-  Route::group(['prefix' => 'settings'], function () {
+  Route::group(['prefix' => 'manage','middleware' => ['permission:manage_inventory']], function () {
     Route::get('stores', InvStoresComponent::class)->name('inventory-stores');
     Route::get('rejection-reasons', RejectionReasons::class)->name('rejection-reasons');
     Route::get('categories', InvCategoriesComponent::class)->name('inventory-categories');
+    Route::get('commodities', CommoditiesComponent::class)->name('inventory-commodities');
+    Route::get('department-items', DepartmentItemsComponent::class)->name('department-items');
+    // Route::get('stores/sections', InvStorageSectionsComponent::class)->name('inventory-sections');
+    // Route::get('stores/sections/bins', InvStorageSubSectionsComponent::class)->name('inventory-storage_bins');
+    // Route::get('unit_of_measures', InvUnitOfMeasuresComponent::class)->name('inventory-unit_of_measures');
+  });
+
+  Route::group(['prefix' => 'manage-stock','middleware' => ['permission:access_stock_management']], function () {
     Route::get('stores/sections', InvStorageSectionsComponent::class)->name('inventory-sections');
     Route::get('stores/sections/bins', InvStorageSubSectionsComponent::class)->name('inventory-storage_bins');
     Route::get('unit_of_measures', InvUnitOfMeasuresComponent::class)->name('inventory-unit_of_measures');
   });
 
-  Route::group(['prefix' => 'requisitions'], function () {
+  Route::group(['prefix' => 'requisitions','middleware' => ['permission:access_department_request']], function () {
     Route::get('forecast', ForecastsComponent::class)->name('forecast');
     Route::get('general-requests', GeneralRequisitionsComponent::class)->name('general-requests');
     Route::get('incoming-requests', GeneralRequisitionsComponent::class)->name('incoming-requests');
