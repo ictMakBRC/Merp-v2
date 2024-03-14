0658cd372d1415cd9c7c75e03a3f9aa0c59e237e
ked-k <kedkayz@gmail.com>
2024-03-14 14:39:16 +0300

    Update FmsClientsComponent.php

--- app/Http/Livewire/Finance/Customer/FmsClientsComponent.php
+++ app/Http/Livewire/Finance/Customer/FmsClientsComponent.php
@@ -103,7 +103,7 @@ class FmsClientsComponent extends Component
             'title' => 'nullable|string',
             'currency_id' => 'required|integer',
             'name' => 'required|string',
-            'code' => 'required|string',
+            'code' => 'nullable|string|max:10',
             'type' => 'required|string',
             'gender' => 'nullable|string',
             'parent_id' => 'nullable|integer',
@@ -139,7 +139,7 @@ class FmsClientsComponent extends Component
         $customer->name = $this->name;
         $customer->parent_id = $this->parent_id;
         $customer->currency_id = $this->currency_id;
-        $customer->code = $this->code;
+        $customer->code = $this->code?? GeneratorService::generateInitials($this->name);
         $customer->type = $this->type ?? 'Customer';
         $customer->nationality = $this->nationality;
         $customer->address = $this->address;
